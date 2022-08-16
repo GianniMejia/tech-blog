d9c2959c80b598d654730a02aeac5225f6652d6f
GianniMejia <101169251+GianniMejia@users.noreply.github.com>
2022-08-15 22:44:11 -0700

    updated hadlebars signup and login

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ db.sync();
 
 const app = express();
 
+app.use(express.json());
+
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./views");
@@ -32,6 +34,16 @@ app.get("/", (req, res) => {
       },
     ],
   });
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+app.get("/api/signup", (req, res) => {
+  console.log("body: ", req.body);
+  // TODO: Insert the new user into the database
+  res.status(500).send("test");
 });
 
 app.listen(3002, () => console.log("server running."));
